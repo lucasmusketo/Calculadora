ce1c16a3d544bf3f29db9233349a418fa76e5741
Lucas Musketo <lucafairo@hotmail.com>
2017-09-05 19:48:51 -0300

    Minha primeira aplicação publicada no gitHub

---
+++ src/aula/SegundoPrograma.java
@@ -0,0 +1,10 @@
+package aula;
+
+public class SegundoPrograma {
+
+		public static void main (String args []){
+			//TODO Auto- generated method stub
+			//Teste de publicacao no gitHub
+			System.out.println("publicando o primeiro programa no gitHub");
+		}
+}
